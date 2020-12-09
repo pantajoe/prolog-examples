121922c095762a17ef79f8416d7e57519601caf1
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-12-09 17:47:31 +0100

    add comment to median

--- median.pl
+++ median.pl
@@ -2,6 +2,7 @@
 
 median(L, R) :-
   length(L, Length),
+  % select the lower median if the length of the list is even.
   Index is floor((Length / 2) - 0.5),
   sort(L, SL),
   select(SL, Index, R).
