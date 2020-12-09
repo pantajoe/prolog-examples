5dbed1c6d407542162c84fde944517e15b55bdbb
Joe Pantazidis <joe.pantazidis@outlook.de>
2020-12-09 17:47:41 +0100

    add cheapest_edge

---
+++ cheapest_edge.pl
@@ -0,0 +1,10 @@
+% cheapest_edge(+Edges, -CheapestEdge).
+cheapest_edge([N-C-M], N-C-M).
+cheapest_edge([N-C-M|Edges], N-C-M) :-
+  cheapest_edge(Edges, _-C1-_),
+  C =< C1,
+  !.
+cheapest_edge([_|Edges], N-C-M) :- cheapest_edge(Edges, N-C-M).
+
+% cheapest_edge([n0-3-n1,n0-1-n3,n0-2-n2], CheapestEdge).
+% -> n0-1-n3
